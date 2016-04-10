e2adaf273993927c2bce4d10376408f5f9edfc43
Drew Keller <drew@eightycolumns.com>
2016-04-10 09:24:43 -0400

    Create Babysitter latest_availability accessors

--- spec/babysitter.rb
+++ spec/babysitter.rb
@@ -4,9 +4,14 @@ describe 'The babysitter' do
   before :each do
     @babysitter = Babysitter.new
     @babysitter.earliest_availability = 17
+    @babysitter.latest_availability = 4
   end
 
   it 'starts no earlier than 5:00 p.m.' do
     expect(@babysitter.earliest_availability).to be 17
+  end
+
+  it 'leaves no later than 4:00 a.m.' do
+    expect(@babysitter.latest_availability).to be 4
   end
 end
--- src/babysitter.rb
+++ src/babysitter.rb
@@ -1,3 +1,4 @@
 class Babysitter
   attr_accessor :earliest_availability
+  attr_accessor :latest_availability
 end
