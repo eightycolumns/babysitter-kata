82c34b7bb206b9a9521436c2d7753b77581a47a1
Drew Keller <drew@eightycolumns.com>
2016-04-11 18:48:13 -0400

    Create Babysitter rate_before_bedtime accessors

--- spec/babysitter.rb
+++ spec/babysitter.rb
@@ -5,6 +5,7 @@ describe 'The babysitter' do
     @babysitter = Babysitter.new
     @babysitter.earliest_availability = 17
     @babysitter.latest_availability = 4
+    @babysitter.rate_before_bedtime_in_dollars_per_hour = 12
   end
 
   it 'starts no earlier than 5:00 p.m.' do
@@ -13,5 +14,9 @@ describe 'The babysitter' do
 
   it 'leaves no later than 4:00 a.m.' do
     expect(@babysitter.latest_availability).to be 4
+  end
+
+  it 'makes $12 per hour before bedtime' do
+    expect(@babysitter.rate_before_bedtime_in_dollars_per_hour).to be 12
   end
 end
