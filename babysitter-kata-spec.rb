c8e91124ae5d44c2673ebe2f1ac5145451f99661
Drew Keller <drew@eightycolumns.com>
2016-04-15 07:14:56 -0400

    Add a calculation
    Because the kids might not go to bed until after the babysitter has
    left, add a test with start_time 6:00 p.m., bedtime 10:00 p.m., and
    end_time 9:00 p.m.

--- babysitter-kata-spec.rb
+++ babysitter-kata-spec.rb
@@ -164,5 +164,25 @@ describe 'Babysitter kata' do
 
       expect(@babysitter.nightly_charge_in_dollars(@shift)).to be 112
     end
+
+    it (
+      'is $36 with a start time of 6:00 p.m., ' +
+      'a bedtime of 10:00 p.m., ' +
+      'and an end time of 9:00 p.m.'
+    ) do
+      @shift.start_time = Time.new(
+        @today.year, @today.month, @today.day, 18
+      )
+
+      @shift.bedtime = Time.new(
+        @today.year, @today.month, @today.day, 22
+      )
+
+      @shift.end_time = Time.new(
+        @today.year, @today.month, @today.day, 21
+      )
+
+      expect(@babysitter.nightly_charge_in_dollars(@shift)).to be 36
+    end
   end
 end
